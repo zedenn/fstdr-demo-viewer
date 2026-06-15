3d9295937ce2ddf49825886c0f45881f00d29654
zedenn <tuovisenteemu@gmail.com>
2026-06-15 21:49:39 +0300

    Fix the site path in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zedenn.github.io",
+  site: "https://teemu.dev",
   base: "/fstdr-demo-viewer",
 });
