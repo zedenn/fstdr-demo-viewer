617c655ca3d576fc160f7deac7115606b900bb70
zedenn <tuovisenteemu@gmail.com>
2026-06-15 21:36:33 +0300

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://zedenn.github.io",
+  base: "/fstdr-demo-viewer",
+});
